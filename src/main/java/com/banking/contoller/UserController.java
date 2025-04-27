d5fa64ffb985584b22a7da8b182c373df967a33a
Sham2003 <wolverinesha2003@gmail.com>
2025-04-27 19:55:07 +0530

    Activation

--- src/main/java/com/banking/contoller/UserController.java
+++ src/main/java/com/banking/contoller/UserController.java
@@ -4,7 +4,6 @@ import com.banking.dto.auth.LoginDto;
 import com.banking.dto.auth.LoginResponse;
 import com.banking.dto.auth.ResetPasswordDTO;
 import com.banking.service.UserService;
-import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
